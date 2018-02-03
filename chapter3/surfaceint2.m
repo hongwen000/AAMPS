cd3cbe9c72a0b9ddd7bfd069ee9d1b1ee3588a3c
李新锐 <hongwen000@outlook.com>
2018-02-03 15:09:43 +0800

    commit

--- chapter3/surfaceint2.m
+++ chapter3/surfaceint2.m
@@ -3,4 +3,11 @@ xu = diff(x,u);
 yu = diff(y,u);
 zu = diff(z,u);
 xv = diff(x,v);
-x
+yv = diff(y,v);
+zv = diff(z,v);
+A = yu*zv - zu*yv;
+B = zu*xv - xu*zv;
+C = xu*yv - yu*xv;
+f = A*P + B*Q + C*R;
+I = int(int(f,u,ua,ub),v,va,vb);
+end
